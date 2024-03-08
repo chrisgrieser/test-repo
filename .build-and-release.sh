3c262f8c7bda4ad11f7d1344e58139f596a509f3
Chris Grieser <73286100+chrisgrieser@users.noreply.github.com>
2024-03-08 21:13:23 +0100

    update build script

--- .build-and-release.sh
+++ .build-and-release.sh
@@ -1,8 +1,4 @@
 #!/bin/zsh
-# BUILD AND RELEASE
-#───────────────────────────────────────────────────────────────────────────────
-
-# goto git root
 cd "$(git rev-parse --show-toplevel)" || return 1
 
 # Prompt for next version number
@@ -18,25 +14,22 @@ if [[ -z "$nextVersion" || "$nextVersion" == "$currentVersion" ]]; then
 	exit 1
 fi
 
-# update version number in *repo* info.plist
+#───────────────────────────────────────────────────────────────────────────────
+
+# update version in REPOS info.plist
 plutil -replace version -string "$nextVersion" info.plist
 
-# #───────────────────────────────────────────────────────────────────────────────
 #
-# INFO specific to my setup: update version number in *local* info.plist
+# INFO specific to my setup:
+# update version in LOCAL info.plist
 localInfoPlist="$HOME/.config/Alfred.alfredpreferences/workflows/$(basename "$PWD")/info.plist"
 if [[ -f "$localInfoPlist" ]]; then
 	plutil -replace version -string "$nextVersion" "$localInfoPlist"
 fi
 
-# convenience: copy download link for current version
-workflow_name="$(basename "$PWD")"
-echo -n "https://github.com/chrisgrieser/${workflow_name}/releases/download/${nextVersion}/${workflow_name}.alfredworkflow" |
-	pbcopy
-
 #───────────────────────────────────────────────────────────────────────────────
 
-git add --all &&
+git add "info.plist" &&
 	git commit -m "release: $nextVersion" &&
 	git pull && git push &&
 	git tag "$nextVersion" && git push origin --tags # trigger the release action
